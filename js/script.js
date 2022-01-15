82563bfa85f3443dd1f3dfd53c56ec1af6f0e7a8
Grace Nyutu <graceynyutu@gmail.com>
2022-01-15 20:12:00 +0300

    Add the order form

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-$(document).ready(function(){
-    $("gallery.small").click(function(){
-        $("gallery.description").css("background-color", "red");
-    });
-    $("#medium").click(function(){
-        $("#crusts").show();
-    });$("#large").click(function(){
-        $url("crusts.html").show();
-    });
+/* size prices */
+let smallPizza = 500;
+let mediumPizza = 600;
+let largePizza = 700;
+let xlargePizza = 800;
+/* crusts prices */
+let crispyCrust = document.getElementById("crispyCrust").value;
+let stuffedCrust = document.getElementById("stuffedCrust").value;
+let gfreeCrust = document.getElementById("gFreeCrust").value;
+/* toppings prices */
+let cheddar = document.getElementById("cheddar").value;
+let mushroom = document.getElementById("mushroom").value;
+let pineapple = document.getElementById("pineapple").value;
+let veggies = document.getElementById("veggies").value;
+let rainbow = document.getElementById("rainbow").value;
 
+
+$(document).ready(function(){
+    $("#crusts button").on("change", function(){
+        alert($("button[name=radioName]:checked", "#crusts").val());
+    });
 });
